c8e458a1f10e23d62d6373e4cf439d47d9a552e1
imdatk <71654293+imdatk@users.noreply.github.com>
2020-09-24 12:30:48 +0200

    Add files via upload

---
+++ Assignment1/Question3.java
@@ -0,0 +1,24 @@
+package assignments.day_2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+
+    // Write multiple print statements that will print from 1 to 10 in separate lines.
+
+System.out.println(1);
+System.out.println(2);
+System.out.println(3);
+System.out.println(4);
+System.out.println(5);
+System.out.println(6);
+System.out.println(7);
+System.out.println(8);
+System.out.println(9);
+System.out.println(10);
+
+
+
+
+}
+}
---
+++ Assignment1/Question4.java
@@ -0,0 +1,13 @@
+package assignments.day_2;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// Rearrange the following code so that it forms a correct program that prints out the letter
+		
+		
+	System.out.println("Q");
+
+	}
+
+}
