50b871949b6221ea0f65be2ea393f1ccbe5dca36
imdatk <71654293+imdatk@users.noreply.github.com>
2020-09-24 12:44:46 +0200

    Add files via upload

---
+++ Assignment1/Question8.java
@@ -0,0 +1,26 @@
+package assignments.day_2;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+ 
+		// Write a program that will print your initials to standard output in letters that are nine lines tall. 
+		// Each big letter should be made up of a bunch of *'s. For example, if your initials were "DJE",
+		 
+		System.out.println("****         ****           *****");
+		System.out.println(" **           **           ** ");
+		System.out.println(" **           **         ** ");
+		System.out.println(" **           **       ** ");
+		System.out.println(" **           **     ** ");
+		System.out.println(" **           **   ** ");
+		System.out.println(" **           ** ** ");
+		System.out.println(" **           **   ** ");
+		System.out.println(" **           **     ** ");
+		System.out.println(" **           **       ** ");
+		System.out.println(" **           **         ** ");
+		System.out.println(" **           **           ** ");
+		System.out.println("****         ****            **** ");
+		
+		
+	}
+}
